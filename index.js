7f204c017ff9b8c677e4cbbacd47f4de237f1818
Zander-Davidson <alexander.davidson98@gmail.com>
2019-12-11 19:45:56 -0600

    added clear and saved state filter

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ express()
 
       const result = await client.query('INSERT INTO userdata (user_id, username, password) VALUES (\'' + randId + '\', \'' + req.body.username + '\', \'' + req.body.password + '\')');
       res.render('pages/index', { "results": { "msg": "Success! You can now log in as a user", "msgcolor": "green" } });
-      client.release();
 
     } catch (err) {
       console.error(err);
@@ -71,7 +70,7 @@ express()
     }
   })
 
-  // login
+  // login, routes to courses page on success, or back to homepage on failure
   .post('/courses', async (req, res) => {
     try {
       const client = await pool.connect();
@@ -83,9 +82,13 @@ express()
         var obj = users.rows[i];
         if (obj.username == req.body.username) {
           if (obj.password == req.body.password) {
-            const result = await client.query('SELECT * FROM courses WHERE subject = \'CSCE\'');
-            const results = { 'results': (result) ? result.rows : null };
-            res.render('pages/courses', results)
+
+            const courses = await client.query('SELECT * FROM courses WHERE subject = \'CSCE\'');
+            const subjectdata = await client.query('SELECT * FROM subjectdata');
+            const query = { "subject" : "CSCE", "minhours" : "0", "maxhours" : "20", "level": "" }
+
+            const results = { 'courses': (courses) ? courses.rows : null, 'subjectdata': (subjectdata) ? subjectdata.rows : null, "query" : (query) };
+            res.render('pages/courses', results);
             client.release();
           }
         }
@@ -100,39 +103,48 @@ express()
     }
   })
 
+  // general search request
   .get('/courses', async (req, res) => {
     try {
+      query = req.query
+
       const client = await pool.connect()
 
       // this starter string allows access to the entire table if no query args provided in request
       var str = 'SELECT * FROM courses';
 
-      if (Object.keys(req.query).length > 0) {
+      if (Object.keys(query).length > 0) {
         // one example of a result: SELECT * FROM courses WHERE subject = 'MATH' AND hours = '3'
-        var qual = ''; // qualifier for filter
-        for (var key of Object.keys(req.query)) {
-          if (req.query[key] != '') {
+        var qual = ''; // qualifier string to append to str
+        for (var key of Object.keys(query)) {
+          if (query[key] != '') {
             if (key == 'minhours' || key == 'maxhours') {
               if (key == 'minhours') {
-                qual = qual + ' ' + key + ' >= ' + req.query[key] + ' AND';
+                qual = qual + ' ' + key + ' >= ' + query[key] + ' AND';
               }
               if (key == 'maxhours') {
-                qual = qual + ' ' + key + ' <= ' + req.query[key] + ' AND';
+                qual = qual + ' ' + key + ' <= ' + query[key] + ' AND';
               }
             } else {
-              qual = qual + ' ' + key + ' = \'' + req.query[key] + '\' AND';
+              qual = qual + ' ' + key + ' = \'' + query[key] + '\' AND';
             }
           }
         }
         if (qual.length > 0) {
           str = str + ' WHERE' + qual + ' subject IS NOT NULL'; // could be better. needed to handle the extra 'AND' at the end
         }
+      } else {
+        str = 'SELECT * FROM courses WHERE subject = \'CSCE\''
+        query = { "subject" : "CSCE", "minhours" : "0", "maxhours" : "20", "level": "" }
       }
 
-      const result = await client.query(str);
-      const results = { 'results': (result) ? result.rows : null };
-      res.render('pages/courses', results)
+      const courses = await client.query(str);
+      const subjectdata = await client.query('SELECT * FROM subjectdata');
+      
+      const results = { "courses": (courses) ? courses.rows : null, "subjectdata": (subjectdata) ? subjectdata.rows : null, "query" : (query) };
+      res.render('pages/courses', results);
       client.release();
+
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
